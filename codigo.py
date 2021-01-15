ef2b21a4ece79d99f9d5fee3525f4a4e37b0801c
Pernalonga30 <davipernalonga30@gmail.com>
2021-01-15 10:56:41 -0300

    primeiro commit

--- codigo.py
+++ codigo.py
@@ -1,5 +1,8 @@
 class Personagem:
-    def __init__(self):
-        self._nome_personagem = {}
-    def criar_personagem(self):
-        if 
+    def __init__(self, tipo_personagem, nome_personagem, pontos_vidas, pontos_defesa, porcentagem_powerup):
+        self._tipo_personagem = tipo_personagem
+        self._nome_personagem = nome_personagem
+        self.pontos_vidas = pontos_vidas
+        self.pontos_defesa = pontos_defesa
+        self.porcentagem_powerup = porcentagem_powerup
+

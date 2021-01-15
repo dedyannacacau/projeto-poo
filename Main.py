81909cb6206c824bd0445f3ac7ecee9d1da784cf
João Pedro Ricarte <joaopedroricarte@hotmail.com>
2021-01-15 16:45:48 -0300

    editar personagem

--- Main.py
+++ Main.py
@@ -49,6 +49,43 @@ class Main:
             if personagem.nome_personagem == personagemExcluido:
                 array.remove(personagem)
                
+    def editar_personagem(array):
+        i = 0
+        print('Qual personagem deseja editar?')
+
+        for personagem in array:
+            print('\n')
+            print('Classe: ' + personagem.tipo_personagem)
+            print('Nome: ' + personagem.nome_personagem)
+            print('Vidas: ' + str(personagem.pontos_vidas))
+            print('Ataque: ' + str(personagem.pontos_ataque))
+            print('Defesa: ' + str(personagem.pontos_defesa))
+            print('Power-Up: ' + str(personagem.porcentagem_powerup) + "%")
+            print('\n')
+
+        personagemEditado = input('Digite o nome do Personagem que deseja editar: ')
+
+        for personagem in array:
+            if personagem.nome_personagem == personagemEditado:
+                nome_personagem = input('Nome do personagem: ')
+                for personagem in array:
+                    if personagem.nome_personagem == nome_personagem:
+                        print('Personagem já existe!')
+                        nome_personagem = input('Nome do personagem: ')
+                    else:
+                        break          
+                tipo_personagem = input('Classe do personagem: ')
+                pontos_vidas = int(input('Coloque os pontos de VIDA do personagem: '))
+                pontos_ataque = int(input('Coloque o valor do ATAQUE do personagem: '))
+                pontos_defesa = int(input('Coloque os pontos de DEFESA do personagem: '))
+                porcentagem_powerup = int(input('Coloque, em porcentagem, o valor de POWERUP do personagem: '))
+
+                personagem = Personagem(tipo_personagem, nome_personagem, pontos_vidas, pontos_ataque, pontos_defesa, porcentagem_powerup)
+                array[i] = personagem
+            else:
+                i = i + 1     
+                    
+
 
 
 
@@ -57,7 +94,7 @@ class Main:
     opcao = ''
 
     while opcao != 0:
-        print('1- Criar Personagem ')
+        print('\n1- Criar Personagem ')
         print('2- Mostrar Personagens')
         print('3- Editar Personagem')
         print('4- Excluir Personagem')
@@ -69,6 +106,8 @@ class Main:
         if opcao == 1:
             criar_personagem(personagens)
         elif opcao == 2:
-            mostrar_personagens(personagens)    
+            mostrar_personagens(personagens)
+        elif opcao == 3:
+            editar_personagem(personagens)        
         elif opcao == 4:
             excluir_personagem(personagens)   

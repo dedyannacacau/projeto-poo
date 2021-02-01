e2f36473ba20583315d7e7e3a6c80a1a1357ae45
Davi de Paula Coelhor Raia dos Santos <davipernalonga30@gmail.com>
2021-01-31 23:57:34 -0300

    Update Arqueiro.py

--- Arqueiro.py
+++ Arqueiro.py
@@ -2,6 +2,8 @@ from Personagem import Personagem
 
 
 class Arqueiro(Personagem):
-    def __init__(self, nome_personagem, pontos_vidas, pontos_ataque, pontos_defesa, porcentagem_powerup)
-    super().__init__(nome_personagem, pontos_vidas,
-                     pontos_ataque, pontos_defesa, porcentagem_powerup)
+    def hab_especial(array, array2):
+        personagem_atacado = input("\nColoque o nome do personagem que deseja atirar uma flecha flamejante com o dano 50 vezes maior que o normal: ")
+        for personagem_atacado in array:
+            personagem_atacado.pontos_vidas =- 50*personagem_da_vez.pontos_ataque
+        

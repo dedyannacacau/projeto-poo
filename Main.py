30f71cdf8227e0cd25ee4000a725720666468ee4
Davi de Paula Coelhor Raia dos Santos <davipernalonga30@gmail.com>
2021-01-31 23:59:20 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 from Personagem import Personagem
 from Funcionalidades import Funcionalidade
+from Batalha import Batalha
 import json
-
-
-class Main(Funcionalidade):
+class Main(Funcionalidade, Batalha):
     personagens = []
     opcao = ''
+    lista = []
     while opcao != 0:
         print('\n1- Criar Personagem')
         print('2- Mostrar Personagens')
         print('3- Editar Personagem')
         print('4- Excluir Personagem')
         print('5- Salvar os Personagens')
-        print('6- Carregar Personagens')
-        print('7- Realizar batalha')
+        print('6- Carregar os Personagens')
+        print('7- Realizar uma Batalha')
         print('0- Sair')
 
         opcao = int(input())
@@ -31,4 +31,6 @@ class Main(Funcionalidade):
         elif opcao == 5:
             Funcionalidade.salvar_personagem_json(personagens)
         elif opcao == 6:
-            Funcionalidade.carregar_personagem_json(personagens)
+            Funcionalidade.carregar_personagem_json(lista, personagens)
+        elif opcao == 7:
+            

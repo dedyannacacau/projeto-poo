fd6a7f4cc6ba68b25b87182362d6afc8e39918e6
Davi de Paula Coelho Raia dos Santos <davipernalonga30@gmail.com>
2021-02-01 08:10:39 -0300

    Update Batalha.py

--- Batalha.py
+++ Batalha.py
@@ -3,57 +3,66 @@ from Personagem import Personagem
 from random import randint
 
 class Batalha(Funcionalidade, Personagem):
-    personagem_batalha = []
-    personagem_batalha2 = []
-    def escolher_personagens(array):
-        i = -1
+    def escolher_personagens(array, personagem_batalha):
+        i = 0
         print("\nselecione os personagens que irão participar da batalha, minimo de 2 personagens.\n")
-        while i != len(array) - 1:
-            i =+ 1
-            personagem_escolhido = input("Coloque o nome do personagem aqui: ")
+        personagem_escolhido = ""
+        while personagem_escolhido != "0":
+            personagem_escolhido = input("Coloque o nome do personagem aqui. Coloque 0 para sair: ")
             for personagem in array:
+                print(array)
+                print(personagem.nome_personagem)
                 #personagem_escolhido = input("Coloque o nome do personagem aqui: ")
-                if personagem_escolhido == personagem.nome_personagem:
-                    personagens_batalha = personagem_escolhido
-                else:
-                    print("Personagem não existe")
+                if str(personagem.nome_personagem) == personagem_escolhido:
+                    personagem_batalha.append(personagem)
+                    print(personagem_batalha)
                     break
-            if len(personagens_batalha) == 0:
+                #precisa de excessão aqui!! se colocar um personagem que não existe!!!!!
+            if len(personagem_batalha) == 0:
                 print("\nPersonagem não existe")
-                #criar excessão aqui!
+                
+        i =+ 1
             
-    def batalha(array):
-        while len(personagens_batalha) != 0:
-            Funcionalide.verificar_pv_personagem(personagens_batalha) 
-            personagens_batalha2 = personagens_batalha
+    def batalha(array, personagem_batalha, personagem_batalha2):
+        while len(personagem_batalha) != 1:
+            Funcionalidade.verificar_pv_personagem(personagem_batalha)
+            personagem_batalha2 = personagem_batalha
             i = 0
-            while len(personagens_batalha2) != 0:
-                Funcionalide.verificar_pv_personagem(personagens_batalha)
-                i = randint(0, len(personagens_batalha2))
-                for personagens_batalha2[i] in array:
-                    personagem_da_vez = personagens_batalha2
+            for personagem in personagem_batalha:
+                personagem.atacar = False
+                personagem.defender = False
+                personagem.powerup = False
+            while len(personagem_batalha2) != 0:
+                Funcionalidade.verificar_pv_personagem(personagem_batalha)
+                i = randint(0, len(personagem_batalha2)-1)
+                for personagem_batalha2[i] in array:
+                    personagem_da_vez = personagem_batalha2[i]
                     op = ''
-                    while op != 0:
-                        print(personagem_da_vez)
+                    while op != '0':
+                        print(personagem_da_vez.nome_personagem)
                         print("1- Atacar")
                         print("2- Defender")
                         print("3- Power up")
                         print("4- Habilidade Especial, NOTA: Cada personagem só pode usa-la uma vez por batalha.")
+                        print("0- Para sair")
 
                         op = input()
                         if op == 1:
-                            Funcionalide.verificar_pv_personagem(personagens_batalha)
-                            Funcionalidade.ataque(personagens_batalha, personagens_batalha2)
+                            Funcionalidade.verificar_pv_personagem(personagem_batalha)
+                            Funcionalidade.ataque(personagem_batalha2[i],personagem_batalha, personagem_batalha2)
                         elif op == 2:
-                            Funcionalide.verificar_pv_personagem(personagens_batalha)
-                            Funcionalidade.defesa(personagens_batalha, personagens_batalha2)
+                            Funcionalidade.verificar_pv_personagem(personagem_batalha)
+                            Funcionalidade.defesa(personagem_batalha2[i],personagem_batalha, personagem_batalha2)
                         elif op == 3:
-                            Funcionalide.verificar_pv_personagem(personagens_batalha)
-                            Funcionalidade.powerup(personagens_batalha, personagens_batalha2)
+                            Funcionalidade.verificar_pv_personagem(personagem_batalha)
+                            Funcionalidade.powerup(personagem_batalha2[i],personagem_batalha, personagem_batalha2)
                         elif op == 4:
-                            Funcionalide.verificar_pv_personagem(personagens_batalha)
-                            Funcionalidade.hab_especial(personagens_batalha, personagens_batalha2)
+                            Funcionalidade.verificar_pv_personagem(personagem_batalha)
+                            Funcionalidade.hab_especial(personagem_batalha2[i],personagem_batalha, personagem_batalha2)
                         op = 0
+        for personagem in personagem_batalha:
+            print(personagem.nome_personagem + "é o vencedor!!")
+            opcao = 0
             
 
 

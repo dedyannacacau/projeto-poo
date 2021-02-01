858a7ecdc243402f5b00665006f5e8fe23fc4295
Davi de Paula Coelhor Raia dos Santos <davipernalonga30@gmail.com>
2021-01-31 23:57:56 -0300

    Update Bruxo.py

--- Bruxo.py
+++ Bruxo.py
@@ -2,6 +2,10 @@ from Personagem import Personagem
 
 
 class Bruxo(Personagem):
-    def __init__(self, nome_personagem, pontos_vidas, pontos_ataque, pontos_defesa, porcentagem_powerup)
-    super().__init__(nome_personagem, pontos_vidas,
-                     pontos_ataque, pontos_defesa, porcentagem_powerup)
+    def hab_especial(array, array2):
+        personagem_atacado = input("\nColoque o nome do personagem que deseja congelar e remover da batalha: ")
+        for personagem_atacado in array:
+            array.remove(personagem_atacado)
+        for personagem_atacado in array2:
+            array.remove(personagem_atacado)
+
